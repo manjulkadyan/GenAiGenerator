1297d558b18e59579dc543395b93f71db268966a
Manjul Kadyan <manjulkadyan@gmail.com>
2025-11-24 20:33:43 +0530

    Update Gradle plugins for Kotlin 2.1.0 and migrate from KAPT to KSP for Room compatibility

--- genai-android/build.gradle.kts
+++ genai-android/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.28" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
