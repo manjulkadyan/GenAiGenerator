d9d9178cb7f7f41b568b33f11d51da25f6b2129a
Manjul Kadyan <manjulkadyan@gmail.com>
2026-01-12 15:29:41 +0530

    Bump version code to 12 and update version name to 0.1.2

--- genai-android/app/build.gradle.kts
+++ genai-android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.manjul.genai.videogenerator"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.1.1"
+        versionCode = 12
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
