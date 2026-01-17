7954394a7fd8dbfcbb0672f22e258e9a8163065a
Manjul Kadyan <manjulkadyan@gmail.com>
2026-01-17 11:58:08 +0530

    Bump version code to 15 and update version name to 0.1.5

--- genai-android/app/build.gradle.kts
+++ genai-android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.manjul.genai.videogenerator"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.1.4"
+        versionCode = 15
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
