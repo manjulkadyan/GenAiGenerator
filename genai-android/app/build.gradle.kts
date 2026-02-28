ae5030beb0c97707e05d02847764d2074919f318
Manjul Kadyan <manjulkadyan@gmail.com>
2026-02-28 16:45:49 +0530

    0.1.6

--- genai-android/app/build.gradle.kts
+++ genai-android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.manjul.genai.videogenerator"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.1.5"
+        versionCode = 16
+        versionName = "0.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
